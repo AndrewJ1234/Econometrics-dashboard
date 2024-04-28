401907974150d37b4eb3072b7c45308abe0e993e
AndrewJ1234 <aj3556@nyu.edu>
2024-04-28 17:06:51 -0400

    added a csv file containing the data that needed to be displayed. Also I successfully displayed the data in 7 different line charts.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+import fact_environmental_data from "./fact_environmental_data.csv";
+import Papa from 'papaparse';
+import {Line} from 'react-chartjs-2'; // used to create line chart
+
+ChartJS.register(
+    CategoryScale, 
+    LinearScale, 
+    PointElement, 
+    LineElement,
+    Title,
+    Tooltip,   
+    Legend,
+);
 
 function App() {
+  const [chartData, setChartData] = useState({ labels: [], datasets: [] });
+  const [isLoading, setIsLoading ] = useState(true);
+
+  useEffect(() => {
+    const fetchParseData = async () => {
+      Papa.parse(fact_environmental_data, {
+        download: true,
+        header: true,
+        delimiter: ",", // sets field delimiter to ","
+        dynamicTyping: true, // convert num/bool input as their respective types
+        complete: (result) => {
+          // Get the months from the fields array in the meta object
+          let labels = result.meta.fields.slice(1); // Exclude the first field which is an empty string
+
+          let datasets = result.data.map((row) => {
+            // Get the type of data from the first property in the row
+            let label = Object.values(row)[0];
+
+            // Get the amounts for each month from the rest of the properties in the row
+            let data = Object.values(row).slice(1);
+
+            return {
+              label: label,
+              data: data,
+              borderColor: "black",
+              backgroundColor: "red"
+            };
+          });
+            
+          setChartData({ 
+            labels: labels, 
+            datasets: datasets
+          });
+
+          setIsLoading(false);
+        },
+      });
+    };
+    fetchParseData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        chartData.datasets.map((dataset, index) => (
+          <div key={index}>
+            <Line options={{ responsive: true }} data={{ labels: chartData.labels, datasets: [dataset] }} />
+          </div>
+        ))
+      )}
     </div>
   );
 }
-
 export default App;
